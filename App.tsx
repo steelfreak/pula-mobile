0b79deb800cf057499716f0db3b145c643f563a3
Fongoh M. Tayong <echzeus@Fongohs-MacBook-Pro-2.local>
2025-08-09 11:03:32 +0100

    feat: scaffolded code with nativebase

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
+import { Container } from 'components/Container';
 import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
+import { Text } from 'react-native';
 
 import './global.css';
 
 export default function App() {
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
       <StatusBar style="auto" />
+      <Container>
+        <ScreenContent title="Home" path="App.tsx">
+          <Text>Hello</Text>
+        </ScreenContent>
+      </Container>
     </>
   );
 }
